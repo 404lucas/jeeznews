baf994bd6b9ed92a3953465831f0c54689c0feb0
404lucas <404lucas.ferreira@gmail.com>
2023-12-04 17:22:37 -0300

    get all and get by id

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,5 +2,7 @@ const route = require("express").Router(); // Declarando router da express diret
 const userController = require('../controllers/user.controller');
 
 route.post("/", userController.create); //Método post da route para executar função post na nossa API...
+route.get("/", userController.findAll);
+route.get("/:id", userController.findById);
 
 module.exports = route; // Exportar função geral
